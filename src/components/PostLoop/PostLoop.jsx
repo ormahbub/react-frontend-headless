be92372bec897bad5507b9a5cee44786d7850557
Mahbub <72622711+ormahbub@users.noreply.github.com>
2026-02-04 18:05:08 +0600

    Post loop has been created and added the posts section.

--- src/components/PostLoop/PostLoop.jsx
+++ src/components/PostLoop/PostLoop.jsx
@@ -1,9 +1,18 @@
 import "./PostLoop.css";
 
 const PostLoop = () => {
-  return <div className="blog-post">
-    .blog-image
-  </div>;
+  return (
+    <a href="#" className="blog-post">
+      <div className="blog-image"></div>
+      <h3 className="blog-title">How to make a wordpress website</h3>
+      <p className="blog-details">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A cum totam
+        consectetur modi quas provident ducimus quis itaque perspiciatis harum
+        nam corporis inventore libero, nostrum fugiat asperiores blanditiis
+        incidunt corrupti!
+      </p>
+    </a>
+  );
 };
 
 export default PostLoop;
--- src/pages/Home/RecentPosts/RecentPosts.jsx
+++ src/pages/Home/RecentPosts/RecentPosts.jsx
@@ -3,13 +3,15 @@ import "./RecentPosts.css";
 
 const RecentPosts = () => {
   return (
-    <div className="recent-posts">
+    <div className="recent-posts container">
       <div className="section-head">
         <h3>Recent Posts</h3>
         <p>Find all of our recent posts here.</p>
       </div>
       <div className="section-content">
         <div className="blog-posts">
+          <PostLoop />
+          <PostLoop />
           <PostLoop />
         </div>
       </div>
