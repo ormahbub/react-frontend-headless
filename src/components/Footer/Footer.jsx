2308ab525971aa7b7e48bd78530cb215fc80fe63
Mahbub <72622711+ormahbub@users.noreply.github.com>
2026-02-07 19:45:18 +0600

    Footer content added and design completed.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,26 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer" id="footer">
-      <p>This is footer text.</p>
+      <div className="container">
+        <div className="footer-left">
+          <ul className="social-list">
+            <li>
+              <a href="#">Facebook</a>
+            </li>
+            <li>
+              <a href="#">LinkedIn</a>
+            </li>
+            <li>
+              <a href="#">YouTube</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-right">
+          <p className="footer-copyright">
+            &copy; {new Date().getFullYear()} - All reserved by Mahbub
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
