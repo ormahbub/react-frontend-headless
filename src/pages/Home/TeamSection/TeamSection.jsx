3083e7bc0dec56f0081a88b702191d4f9945da21
Mahbub <72622711+ormahbub@users.noreply.github.com>
2026-02-08 21:35:57 +0600

    Left content of the team section is added.

--- src/pages/Home/TeamSection/TeamSection.jsx
+++ src/pages/Home/TeamSection/TeamSection.jsx
@@ -5,7 +5,18 @@ const TeamSection = () => {
   return (
     <section className="team-section">
       <div className="container">
-        <div className="left-content"></div>
+        <div className="left-content">
+          <p className="section-subtitle">Exclusive Services</p>
+          <h3 className="section-title">A Team Of Experts You Can Trust</h3>
+          <p className="text-content">
+            Himenaeos mus porttitor pretium rutrum quisque efficitur quisque
+            libero tristique ultricies himenaeos donec ad tincidunt magna.
+          </p>
+          <p className="text-content">
+            Condimentum netus feugiat risus porttitor viverra, suscipit sociosqu
+            bibendum rutrum placerat cras aliquam.
+          </p>
+        </div>
         <div className="right-content">
           <div className="image-box">
             <img src={chartImage} alt="Chart" />
